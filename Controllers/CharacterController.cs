fec2ea28d712a589f59e6ced528c212715bcbc3a
72Roman <27romario@gmail.com>
2020-10-28 20:45:52 +0200

    add dtos, services

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,28 +2,35 @@ using Microsoft.AspNetCore.Mvc;
 using dotnet_rpg.Models;
 using System.Collections.Generic;
 using System.Linq;
+using dotnet_rpg.Services.CharacterService;
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
+
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class CharacterController : ControllerBase
-    {
-        private static List<Character> characters = new List<Character>{
-            new Character(),
-            new Character{Id = 1, Name = "Sam"}
-        };
+    {   
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
         [HttpGet("GetAll")]
-        public IActionResult Get() {
-            return Ok(characters);
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
         }
         [HttpGet("{id}")]
-        public IActionResult GetSingle(int id) {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
         }
         [HttpPost]
-        public IActionResult AddCharacter( Character newCharacter) {
-            characters.Add(newCharacter);
-            return Ok(characters);
+        public async Task<IActionResult> AddCharacter(AddCharacterDto newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
