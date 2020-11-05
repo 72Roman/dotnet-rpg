0ebdd78c160884f59d0124ab91daae2ad9ff2c11
72Roman <27romario@gmail.com>
2020-11-05 14:00:27 +0200

    finish authentication

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using dotnet_rpg.Services.CharacterService;
 using System.Threading.Tasks;
 using dotnet_rpg.Dtos.Character;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace dotnet_rpg.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class CharacterController : ControllerBase
@@ -20,7 +23,8 @@ namespace dotnet_rpg.Controllers
         [HttpGet("GetAll")]
         public async Task<IActionResult> Get()
         {
-            return Ok(await _characterService.GetAllCharacters());
+            int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            return Ok(await _characterService.GetAllCharacters(userId));
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetSingle(int id)
